52131adb6a4d6f05dc7c8157cb7b6a0b0423672b
klelsonrodrigues <117690197+klelsonrodrigues@users.noreply.github.com>
2024-07-23 21:03:35 -0300

    Add files via upload

---
+++ Klelson rodrigues.js
@@ -0,0 +1,4 @@
+console.log("Olá, Mundo!");
+console.log("Me chamo klelson rodrigues, tenho 32 anos e moro em brasilia  ");
+console.log("gosto de rock e futebol  ");
+console.log("gosto jogar jogos eletronicos ");
